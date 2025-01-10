38bd75cb0b72ef8e98a24747353fc4ce97083f05
WillTheWater <willthewater@protonmail.com>
2025-01-10 18:09:42 +0100

    Player Controller Input Added

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -2,6 +2,7 @@
 
 #include "Player/AuraPlayerController.h"
 #include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
 
 
 AAuraPlayerController::AAuraPlayerController()
@@ -27,4 +28,29 @@ void AAuraPlayerController::BeginPlay()
 	InputModeData.SetHideCursorDuringCapture(false);
 	SetInputMode(InputModeData);
 
+}
+
+void AAuraPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent);
+	EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AAuraPlayerController::Move);
+}
+
+void AAuraPlayerController::Move(const FInputActionValue& InputValue)
+{
+	const FVector2D InputAxis = InputValue.Get<FVector2D>();
+	const FRotator Rotation = GetControlRotation();
+	const FRotator Yaw(0.f, Rotation.Yaw, 0.f);
+	const FRotator Pitch(Rotation.Pitch, 0.f, 0.f);
+
+	const FVector ForwardVector = FRotationMatrix(Yaw).GetUnitAxis(EAxis::X);
+	const FVector RightVector = FRotationMatrix(Yaw).GetUnitAxis(EAxis::Y);
+
+	if(APawn* ControlledPawn = GetPawn<APawn>())
+	{
+		ControlledPawn->AddMovementInput(ForwardVector, InputAxis.Y);
+		ControlledPawn->AddMovementInput(RightVector, InputAxis.X);
+	}
 }
